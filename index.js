637eda66bca76016d2344022a6695170ad49a37f
petermunene <peter.munene@student.moringaschool.com>
2025-03-10 12:32:37 +0300

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(2)
+}
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+function createFareMultiplier(multiplier){
+    return function fairMultiplier(fair){
+        return fair*multiplier
+    
+    }
+}
+function fareDoubler(fare){
+    return fare*2
+}
+function fareTripler(fare){
+    return fare*3
+}
+function selectDifferentDrivers(arrayOfDrivers, driverSelectorFunction) {
+    return driverSelectorFunction(arrayOfDrivers);
+  }
+    
